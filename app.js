8734873e6958767e83d48647461f29ae682a980b
Gavin Nitta <gshigeto@gmail.com>
2017-02-09 16:40:38 -0700

    Fix string

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ creative2.controller('cookbook', function($scope, $http, $window) {
 
   $scope.search = function () {
     $scope.loading = true;
-    $http.get(`https://api.edamam.com/search?q=${$scope.searchText}&app_id=d2bf1fb2&app_key=d75f521a8637036cfa268cb3a78fd825`)
+    $http.get('https://api.edamam.com/search?q=' + $scope.searchText + '&app_id=d2bf1fb2&app_key=d75f521a8637036cfa268cb3a78fd825')
       .then(resp => {
         $scope.searchedText = $scope.searchText;
         $scope.recipes = resp.data.hits;
