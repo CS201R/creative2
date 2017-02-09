eec87145c4f11b3b482287d5e317ef74cd8b3359
Gavin Nitta <gshigeto@gmail.com>
2017-02-09 16:53:04 -0700

    Finish creative 2

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ creative2.controller('cookbook', function($scope, $http, $window) {
 
   $scope.search = function () {
     $scope.loading = true;
-    $http.get('https://api.edamam.com/search?q=' + $scope.searchText + '&app_id=d2bf1fb2&app_key=d75f521a8637036cfa268cb3a78fd825')
-      .then(resp => {
+    $.getJSON('https://api.edamam.com/search?q=' + $scope.searchText + '&app_id=d2bf1fb2&app_key=d75f521a8637036cfa268cb3a78fd825',
+      function (resp) {
         $scope.searchedText = $scope.searchText;
-        $scope.recipes = resp.data.hits;
+        $scope.recipes = resp.hits;
         $scope.loading = false;
-      });
+        $scope.$apply();
+        console.log(resp);
+      }
+    );
   };
 
   $scope.open = function (url) {
